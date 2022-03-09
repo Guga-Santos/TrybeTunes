7901d9f3f6a8106be1c839d8263225b30bdae271
GugaSantos <guga.santos.guga@gmail.com>
2022-03-09 02:38:26 -0300

    Requisito 13 ok

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,53 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      get: '',
+    };
+  }
+
+  componentDidMount() {
+    this.handleFetch();
+  }
+
+  handleFetch = async () => {
+    this.setState({
+      loading: true,
+    });
+    const get = await getUser();
+    this.setState({
+      loading: false,
+      get,
+    });
+  }
+
   render() {
+    const { loading, get } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
         <p>Profile</p>
+        { loading ? <Loading /> : (
+          <>
+            <img
+              src={ get.image }
+              alt="imagem"
+              data-testid="profile-image"
+            />
+            <Link to="/profile/edit">Editar perfil</Link>
+            <h2>{ get.name }</h2>
+            <h3>{ get.email }</h3>
+            <h4>{ get.description }</h4>
+
+          </>
+        ) }
       </div>
     );
   }
